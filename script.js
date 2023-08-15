2deba5d6371ad03069f75ca99e72c1b7686af27b
glumslug <glumaslug@gmail.com>
2023-08-14 23:46:31 -0400

    randomized starting array index for toggle titles, removed immediate title repeat possibility

--- script.js
+++ script.js
@@ -162,8 +162,14 @@ let titles = [
 ];
 const refSym =
   '<svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="#525252" class="bi bi-arrow-repeat" viewBox="0 0 16 16"><path d="M11.534 7h3.932a.25.25 0 0 1 .192.41l-1.966 2.36a.25.25 0 0 1-.384 0l-1.966-2.36a.25.25 0 0 1 .192-.41zm-11 2h3.932a.25.25 0 0 0 .192-.41L2.692 6.23a.25.25 0 0 0-.384 0L.342 8.59A.25.25 0 0 0 .534 9z"/><path fill-rule="evenodd" d="M8 3c-1.552 0-2.94.707-3.857 1.818a.5.5 0 1 1-.771-.636A6.002 6.002 0 0 1 13.917 7H12.9A5.002 5.002 0 0 0 8 3zM3.1 9a5.002 5.002 0 0 0 8.757 2.182.5.5 0 1 1 .771.636A6.002 6.002 0 0 1 2.083 9H3.1z"/></svg>';
+let prevChoice = "";
 function generateTitle() {
   let choice = titles[Math.floor(Math.random() * 20)];
+  if (choice === prevChoice) {
+    generateTitle();
+    return;
+  }
+  prevChoice = choice;
   let titleContainer = document.getElementById("characterTitle");
   titleContainer.innerHTML = "";
 
@@ -171,19 +177,20 @@ function generateTitle() {
     const cont = document.createElement("div");
     cont.classList.add("rel");
     const p = document.createElement("p");
-    p.innerText = choice[0];
+    let i = Math.floor(Math.random() * choice.length);
+    p.innerText = choice[i];
     p.classList.add("syl", "option");
     const changer = document.createElement("button");
     changer.classList.add("changer");
     changer.innerHTML = refSym;
-    let i = 1;
+
     changer.addEventListener("click", () => {
-      p.innerText = choice[i];
       if (i < choice.length - 1) {
         i++;
       } else {
         i = 0;
       }
+      p.innerText = choice[i];
     });
     cont.appendChild(p);
     cont.appendChild(changer);
